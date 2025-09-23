ThyMYthOS/ada_usb_embedded

Move endpoint buffer management into UDC
This allows more control in the UDC over read and write operations to
hardware buffers.

src/usb-device-control.adb
@@ -33,8 +33,6 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with System.Storage_Elements; use System.Storage_Elements;
 
-with USB.Utils;
-
 package body USB.Device.Control is
 
    function Need_ZLP (Len     : Buffer_Len;
@@ -221,7 +219,7 @@ package body USB.Device.Control is
       This.Ctrl.Len := 0;
       This.Ctrl.Buf := This.Ctrl.Buffer'Address;
 
-      --  The UDC will write data to EP_Out_Addr. Upon transfer complete, we
+      --  The UDC will write data to its own buffer. Upon transfer complete, we
       --  will copy to internal control buffer.
 
       if Req.Length > UInt16 (This.Max_Packet_Size) then
@@ -453,9 +451,9 @@ package body USB.Device.Control is
    begin
       if This.Ctrl.Len > Buffer_Len (This.Max_Packet_Size) then
 
-         USB.Utils.Copy (Src   => This.Ctrl.Buf,
-                         Dst   => This.Ctrl.EP_In_Addr,
-                         Count => This.Max_Packet_Size);
+         This.UDC.Move_To_EP (Ep  => 0,
+                              Src => This.Ctrl.Buf,
+                              Len => This.Max_Packet_Size);
 
          This.UDC.EP_Send_Packet (0, This.Max_Packet_Size);
 
@@ -468,9 +466,9 @@ package body USB.Device.Control is
 
       else
          --  Copy data to the UDC EP buffer
-         USB.Utils.Copy (Src   => This.Ctrl.Buf,
-                         Dst   => This.Ctrl.EP_In_Addr,
-                         Count => Packet_Size (This.Ctrl.Len));
+         This.UDC.Move_To_EP (Ep  => 0,
+                              Src => This.Ctrl.Buf,
+                              Len => Packet_Size (This.Ctrl.Len));
 
          This.UDC.EP_Send_Packet (0, Packet_Size (This.Ctrl.Len));
 
@@ -496,9 +494,9 @@ package body USB.Device.Control is
                         Buffer_Len (This.Ctrl.Req.Length) - This.Ctrl.Len);
    begin
       --  Copy data from the UDC EP buffer
-      USB.Utils.Copy (Src   => This.Ctrl.EP_Out_Addr,
-                      Dst   => This.Ctrl.Buf,
-                      Count => Packet_Size (Read_Size));
+      This.UDC.Move_From_EP (Ep  => 0,
+                             Dst => This.Ctrl.Buf,
+                             Len => Packet_Size (Read_Size));
 
       This.Ctrl.Len := This.Ctrl.Len + Read_Size;
       This.Ctrl.Buf := This.Ctrl.Buf + Read_Size;

src/usb-device-hid.adb
@@ -61,9 +61,7 @@ package body USB.Device.HID is
          return Not_Enough_EPs;
       end if;
 
-      This.Report_Buf := Dev.Request_Buffer ((This.EP, EP_In),
-                                             This.Report_Size);
-      if This.Report_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.EP, EP_In), This.Report_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -272,14 +270,13 @@ package body USB.Device.HID is
    procedure Send_Report (This : in out Abstract_HID_Class;
                           UDC  : in out USB_Device_Controller'Class)
    is
-      Report : Report_Data (This.Report'Range)
-        with Address => This.Report_Buf;
-
    begin
       if This.Ready then
 
          --  Copy the report to the transfer buffer
-         Report := This.Report;
+         UDC.Move_To_EP (Ep  => This.EP,
+                         Src => This.Report'Address,
+                         Len => This.Report_Size);
 
          --  Clear report
          This.Report := (others => 0);

src/usb-device-midi.adb
@@ -164,9 +164,9 @@ package body USB.Device.MIDI is
       if State (RG) = Valid then
 
          --  Copy into IN buffer
-         USB.Utils.Copy (Src   => Slice (RG).Addr,
-                         Dst   => This.EP_In_Buf,
-                         Count => Natural (Slice (RG).Length));
+         UDC.Move_To_EP (Ep  => This.EP,
+                         Src => Slice (RG).Addr,
+                         Len => Packet_Size (Slice (RG).Length));
 
          USB.Logging.Device.Log_MIDI_Write_Packet;
 
@@ -198,15 +198,11 @@ package body USB.Device.MIDI is
          return Not_Enough_EPs;
       end if;
 
-      This.EP_Out_Buf := Dev.Request_Buffer ((This.EP, EP_Out),
-                                             EP_Buffer_Size);
-      if This.EP_Out_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.EP, EP_Out), EP_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
-      This.EP_In_Buf := Dev.Request_Buffer ((This.EP, EP_In),
-                                            EP_Buffer_Size);
-      if This.EP_In_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.EP, EP_In), EP_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -510,9 +506,9 @@ package body USB.Device.MIDI is
 
             if State (WG) = Valid then
 
-               USB.Utils.Copy (Src   => This.EP_Out_Buf,
-                               Dst   => Slice (WG).Addr,
-                               Count => CNT);
+               UDC.Move_From_EP (Ep  => This.EP,
+                                 Dst => Slice (WG).Addr,
+                                 Len => CNT);
 
                Commit (This.RX_Queue, WG, BBqueue.Count (CNT));
             else

src/usb-device-serial.adb
@@ -70,9 +70,7 @@ package body USB.Device.Serial is
          return Not_Enough_EPs;
       end if;
 
-      This.Int_Buf := Dev.Request_Buffer ((This.Int_EP, EP_In),
-                                          Irq_Buffer_Size);
-      if This.Int_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.Int_EP, EP_In), Irq_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -82,15 +80,11 @@ package body USB.Device.Serial is
          return Not_Enough_EPs;
       end if;
 
-      This.Bulk_Out_Buf := Dev.Request_Buffer ((This.Bulk_EP, EP_Out),
-                                               Bulk_Buffer_Size);
-      if This.Bulk_Out_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.Bulk_EP, EP_Out), Bulk_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
-      This.Bulk_In_Buf := Dev.Request_Buffer ((This.Bulk_EP, EP_In),
-                                              Bulk_Buffer_Size);
-      if This.Bulk_In_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.Bulk_EP, EP_In), Bulk_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -352,9 +346,9 @@ package body USB.Device.Serial is
 
             if State (WG) = Valid then
 
-               USB.Utils.Copy (Src   => This.Bulk_Out_Buf,
-                               Dst   => Slice (WG).Addr,
-                               Count => CNT);
+               UDC.Move_From_EP (Ep  => This.Bulk_EP,
+                                 Dst => Slice (WG).Addr,
+                                 Len => CNT);
 
                Commit (This.RX_Queue, WG, BBqueue.Count (CNT));
             end if;
@@ -412,9 +406,9 @@ package body USB.Device.Serial is
       if State (RG) = Valid then
 
          --  Copy into IN buffer
-         USB.Utils.Copy (Src   => Slice (RG).Addr,
-                         Dst   => This.Bulk_In_Buf,
-                         Count => Natural (Slice (RG).Length));
+         UDC.Move_To_EP (Ep  => This.Bulk_EP,
+                         Src => Slice (RG).Addr,
+                         Len => Packet_Size (Slice (RG).Length));
 
          USB.Logging.Device.Log_Serial_Write_Packet;
 

src/usb-hal-device.ads
@@ -69,12 +69,24 @@ package USB.HAL.Device is
    function Request_Buffer (This          : in out USB_Device_Controller;
                             Ep            :        EP_Addr;
                             Len           :        Packet_Size)
-                            return System.Address
+                            return Boolean
    is abstract;
    --  Allocate a buffer for the given End-Point, either from RAM or internal
    --  USB Controller memory depending on the controller. UDC must keep track
    --  of the return address to perform IN/OUT transfers (see EP_Send_Packet
    --  and EP_Ready_For_Data).
+
+   procedure Move_To_EP (This : in out USB_Device_Controller;
+                         Ep   : EP_Id;
+                         Src  : System.Address;
+                         Len  : Packet_Size)
+   is abstract;
+
+   procedure Move_From_EP (This : in out USB_Device_Controller;
+                           Ep   : EP_Id;
+                           Dst  : System.Address;
+                           Len  : Packet_Size)
+   is abstract;
 
    function Valid_EP_Id (This : in out USB_Device_Controller;
                          EP   :        EP_Id)

tests/src/usb_testing-udc_stub.ads
@@ -87,7 +87,19 @@ package USB_Testing.UDC_Stub is
    function Request_Buffer (This          : in out Controller;
                             Ep            :        EP_Addr;
                             Len           :        Packet_Size)
-                            return System.Address;
+                            return Boolean;
+
+   overriding
+   procedure Move_To_EP (This : in out Controller;
+                         Ep   : EP_Id;
+                         Src  : System.Address;
+                         Len  : Packet_Size);
+
+   overriding
+   procedure Move_From_EP (This : in out Controller;
+                           Ep   : EP_Id;
+                           Dst  : System.Address;
+                           Len  : Packet_Size);
 
    overriding
    function Valid_EP_Id (This : in out Controller;

Fabien-Chouteau/usb_embedded

src/usb-device-control.adb
@@ -33,8 +33,6 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with System.Storage_Elements; use System.Storage_Elements;
 
-with USB.Utils;
-
 package body USB.Device.Control is
 
    function Need_ZLP (Len     : Buffer_Len;
@@ -221,7 +219,7 @@ package body USB.Device.Control is
       This.Ctrl.Len := 0;
       This.Ctrl.Buf := This.Ctrl.Buffer'Address;
 
-      --  The UDC will write data to EP_Out_Addr. Upon transfer complete, we
+      --  The UDC will write data to its own buffer. Upon transfer complete, we
       --  will copy to internal control buffer.
 
       if Req.Length > UInt16 (This.Max_Packet_Size) then
@@ -453,9 +451,9 @@ package body USB.Device.Control is
    begin
       if This.Ctrl.Len > Buffer_Len (This.Max_Packet_Size) then
 
-         USB.Utils.Copy (Src   => This.Ctrl.Buf,
-                         Dst   => This.Ctrl.EP_In_Addr,
-                         Count => This.Max_Packet_Size);
+         This.UDC.Write_To_EP (Ep  => 0,
+                              Src => This.Ctrl.Buf,
+                              Len => This.Max_Packet_Size);
 
          This.UDC.EP_Send_Packet (0, This.Max_Packet_Size);
 
@@ -468,9 +466,9 @@ package body USB.Device.Control is
 
       else
          --  Copy data to the UDC EP buffer
-         USB.Utils.Copy (Src   => This.Ctrl.Buf,
-                         Dst   => This.Ctrl.EP_In_Addr,
-                         Count => Packet_Size (This.Ctrl.Len));
+         This.UDC.Write_To_EP (Ep  => 0,
+                              Src => This.Ctrl.Buf,
+                              Len => Packet_Size (This.Ctrl.Len));
 
          This.UDC.EP_Send_Packet (0, Packet_Size (This.Ctrl.Len));
 
@@ -496,9 +494,9 @@ package body USB.Device.Control is
                         Buffer_Len (This.Ctrl.Req.Length) - This.Ctrl.Len);
    begin
       --  Copy data from the UDC EP buffer
-      USB.Utils.Copy (Src   => This.Ctrl.EP_Out_Addr,
-                      Dst   => This.Ctrl.Buf,
-                      Count => Packet_Size (Read_Size));
+      This.UDC.Read_From_EP (Ep  => 0,
+                             Dst => This.Ctrl.Buf,
+                             Len => Packet_Size (Read_Size));
 
       This.Ctrl.Len := This.Ctrl.Len + Read_Size;
       This.Ctrl.Buf := This.Ctrl.Buf + Read_Size;

src/usb-device-hid.adb
@@ -61,9 +61,7 @@ package body USB.Device.HID is
          return Not_Enough_EPs;
       end if;
 
-      This.Report_Buf := Dev.Request_Buffer ((This.EP, EP_In),
-                                             This.Report_Size);
-      if This.Report_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.EP, EP_In), This.Report_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -272,14 +270,13 @@ package body USB.Device.HID is
    procedure Send_Report (This : in out Abstract_HID_Class;
                           UDC  : in out USB_Device_Controller'Class)
    is
-      Report : Report_Data (This.Report'Range)
-        with Address => This.Report_Buf;
-
    begin
       if This.Ready then
 
          --  Copy the report to the transfer buffer
-         Report := This.Report;
+         UDC.Write_To_EP (Ep  => This.EP,
+                         Src => This.Report'Address,
+                         Len => This.Report_Size);
 
          --  Clear report
          This.Report := (others => 0);

src/usb-device-midi.adb
@@ -164,9 +164,9 @@ package body USB.Device.MIDI is
       if State (RG) = Valid then
 
          --  Copy into IN buffer
-         USB.Utils.Copy (Src   => Slice (RG).Addr,
-                         Dst   => This.EP_In_Buf,
-                         Count => Natural (Slice (RG).Length));
+         UDC.Write_To_EP (Ep  => This.EP,
+                         Src => Slice (RG).Addr,
+                         Len => Packet_Size (Slice (RG).Length));
 
          USB.Logging.Device.Log_MIDI_Write_Packet;
 
@@ -198,15 +198,11 @@ package body USB.Device.MIDI is
          return Not_Enough_EPs;
       end if;
 
-      This.EP_Out_Buf := Dev.Request_Buffer ((This.EP, EP_Out),
-                                             EP_Buffer_Size);
-      if This.EP_Out_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.EP, EP_Out), EP_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
-      This.EP_In_Buf := Dev.Request_Buffer ((This.EP, EP_In),
-                                            EP_Buffer_Size);
-      if This.EP_In_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.EP, EP_In), EP_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -510,9 +506,9 @@ package body USB.Device.MIDI is
 
             if State (WG) = Valid then
 
-               USB.Utils.Copy (Src   => This.EP_Out_Buf,
-                               Dst   => Slice (WG).Addr,
-                               Count => CNT);
+               UDC.Read_From_EP (Ep  => This.EP,
+                                 Dst => Slice (WG).Addr,
+                                 Len => CNT);
 
                Commit (This.RX_Queue, WG, BBqueue.Count (CNT));
             else

src/usb-device-serial.adb
@@ -70,9 +70,7 @@ package body USB.Device.Serial is
          return Not_Enough_EPs;
       end if;
 
-      This.Int_Buf := Dev.Request_Buffer ((This.Int_EP, EP_In),
-                                          Irq_Buffer_Size);
-      if This.Int_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.Int_EP, EP_In), Irq_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -82,15 +80,11 @@ package body USB.Device.Serial is
          return Not_Enough_EPs;
       end if;
 
-      This.Bulk_Out_Buf := Dev.Request_Buffer ((This.Bulk_EP, EP_Out),
-                                               Bulk_Buffer_Size);
-      if This.Bulk_Out_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.Bulk_EP, EP_Out), Bulk_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
-      This.Bulk_In_Buf := Dev.Request_Buffer ((This.Bulk_EP, EP_In),
-                                              Bulk_Buffer_Size);
-      if This.Bulk_In_Buf = System.Null_Address then
+      if not Dev.Request_Buffer ((This.Bulk_EP, EP_In), Bulk_Buffer_Size) then
          return Not_Enough_EP_Buffer;
       end if;
 
@@ -352,9 +346,9 @@ package body USB.Device.Serial is
 
             if State (WG) = Valid then
 
-               USB.Utils.Copy (Src   => This.Bulk_Out_Buf,
-                               Dst   => Slice (WG).Addr,
-                               Count => CNT);
+               UDC.Read_From_EP (Ep  => This.Bulk_EP,
+                                 Dst => Slice (WG).Addr,
+                                 Len => CNT);
 
                Commit (This.RX_Queue, WG, BBqueue.Count (CNT));
             end if;
@@ -412,9 +406,9 @@ package body USB.Device.Serial is
       if State (RG) = Valid then
 
          --  Copy into IN buffer
-         USB.Utils.Copy (Src   => Slice (RG).Addr,
-                         Dst   => This.Bulk_In_Buf,
-                         Count => Natural (Slice (RG).Length));
+         UDC.Write_To_EP (Ep  => This.Bulk_EP,
+                         Src => Slice (RG).Addr,
+                         Len => Packet_Size (Slice (RG).Length));
 
          USB.Logging.Device.Log_Serial_Write_Packet;
 

src/usb-hal-device.ads
@@ -69,12 +69,24 @@ package USB.HAL.Device is
    function Request_Buffer (This          : in out USB_Device_Controller;
                             Ep            :        EP_Addr;
                             Len           :        Packet_Size)
-                            return System.Address
+                            return Boolean
    is abstract;
    --  Allocate a buffer for the given End-Point, either from RAM or internal
    --  USB Controller memory depending on the controller. UDC must keep track
    --  of the return address to perform IN/OUT transfers (see EP_Send_Packet
    --  and EP_Ready_For_Data).
+
+   procedure Write_To_EP (This : in out USB_Device_Controller;
+                         Ep   : EP_Id;
+                         Src  : System.Address;
+                         Len  : Packet_Size)
+   is abstract;
+
+   procedure Read_From_EP (This : in out USB_Device_Controller;
+                           Ep   : EP_Id;
+                           Dst  : System.Address;
+                           Len  : Packet_Size)
+   is abstract;
 
    function Valid_EP_Id (This : in out USB_Device_Controller;
                          EP   :        EP_Id)

tests/src/usb_testing-udc_stub.ads
@@ -87,7 +87,19 @@ package USB_Testing.UDC_Stub is
    function Request_Buffer (This          : in out Controller;
                             Ep            :        EP_Addr;
                             Len           :        Packet_Size)
-                            return System.Address;
+                            return Boolean;
+
+   overriding
+   procedure Write_To_EP (This : in out Controller;
+                         Ep   : EP_Id;
+                         Src  : System.Address;
+                         Len  : Packet_Size);
+
+   overriding
+   procedure Read_From_EP (This : in out Controller;
+                           Ep   : EP_Id;
+                           Dst  : System.Address;
+                           Len  : Packet_Size);
 
    overriding
    function Valid_EP_Id (This : in out Controller;